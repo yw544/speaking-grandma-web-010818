0b9421dde20bc8a734a07a68fe50c62a5878f4cb
York Wang <yw544@nyu.edu>
2018-01-04 00:58:05 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,7 @@ def speak_to_grandma(phrase)
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
-  
+
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
